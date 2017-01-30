2661dfb6de76542e83cca9862f8632d4c45a65e5
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2017-01-30 16:43:45 -0500

    added tournament.sql

--- tournament.py
+++ tournament.py
@@ -1,37 +1,64 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
 
 
+# Connect to the PostgreSQL database.  Returns a database connection.
 def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
 
+# Remove all the match records from the database.
 def deleteMatches():
-    """Remove all the match records from the database."""
+    DELETE_MATCHES_QUERY = "DELETE FROM matches"
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute(DELETE_MATCHES_QUERY)
+    connection.commit()
+    connection.close()
 
 
+# Remove all the player records from the database.
 def deletePlayers():
-    """Remove all the player records from the database."""
+    DELETE_PLAYERS_QUERY = "DELETE FROM players"
 
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute(DELETE_PLAYERS_QUERY)
+    connection.commit()
+    connection.close()
 
+# Returns the number of players currently registered.
+# Example
 def countPlayers():
-    """Returns the number of players currently registered."""
+    COUNT_PLAYERS_QUERY = "SELECT count(*) FROM players"
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute(COUNT_PLAYERS_QUERY)
+    count = connection.fechall()
+    connection.close()
+    return count
 
 
+# Adds a player to the tournament database.
+#
+# The database assigns a unique serial id number for the player.  (This
+# should be handled by your SQL database schema, not in your Python code.)
+#
+# Args:
+#   name: the player's full name (need not be unique).
+# """
 def registerPlayer(name):
-    """Adds a player to the tournament database.
-  
-    The database assigns a unique serial id number for the player.  (This
-    should be handled by your SQL database schema, not in your Python code.)
-  
-    Args:
-      name: the player's full name (need not be unique).
-    """
+
+    REGISTER_PLAYERS_QUERY = "INSERT INTO players (%s)"
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute(REGISTER_PLAYERS_QUERY, (name,))
+    connection.commit()
+    connection.close()
 
 
 def playerStandings():
@@ -47,6 +74,14 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    QUERY = "SELECT name, wins FROM players ORDER By DESC"
+
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute(QUERY)
+    player_standings = cursor.fetchall()
+    connection.close()
+    return player_standings
 
 
 def reportMatch(winner, loser):
@@ -56,16 +91,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +108,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
--- tournament.sql
+++ tournament.sql
@@ -1,9 +1,26 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
+-- 0. Drop database "tournament if it exists"
+DROP DATABASE IF EXISTS tournament;
 
+-- 1. Create database "tournament" and connect to it
+CREATE DATABASE tournament;
+\c tournament
+
+-- 2. Create table "players"
+CREATE TABLE players (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  wins INT DEFAULT 0 CONSTRAINT non_negative_wins CHECK (wins >= 0),
+  loses INT DEFAULT 0 CONSTRAINT non_negative_loses CHECK (loses >= 0)
+);
+
+-- 3. Create table "matches"
+CREATE TABLE matches (
+  id SERIAL PRIMARY KEY,
+  -- reference to a SERIAL datatype must be an int
+  win INT REFERENCES players (id),
+  lose INT REFERENCES players (id)
+);
+
+-- 4. Create a view using "CREATE VIEW"
